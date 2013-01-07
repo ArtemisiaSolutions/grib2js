c6a540312874009042855ef6cefca24ae3af6e36
Nicolas Herment <nherment@gmail.com>
2013-01-07 08:32:09 +0100

    initial commit. reading sections 1, 2 and 3 of grib files

---
+++ test/Grib2Test.js
@@ -0,0 +1,25 @@
+
+var should  = require("should")
+
+var Grib2   = require("../lib/grib/Grib2.js")
+
+describe("When indexing", function() {
+    
+    
+    it("should work", function(done) {
+        
+    this.timeout(60000)
+        // Grib2.index(__dirname + "/data/wgrib.grib2", function(err, index) {
+        Grib2.index(__dirname + "/data/ofs_atl.t00z.f001.grb.grib2", function(err, index) {
+            
+            if(err) throw err
+                
+            console.log(JSON.stringify(index, null, 2))
+                
+            done()
+            
+        })
+        
+    })
+    
+})
