26f032e09d90a1ae345f44d23c246b1f8cfccd6d
Eric Hostalery <hostalerye@gmail.com>
2013-02-04 11:52:02 +0100

    fix issue with Grib2WrapperTest

--- test/Grib2WrapperTest.js
+++ test/Grib2WrapperTest.js
@@ -8,8 +8,8 @@ describe("Verifying if a point is in an area", function() {
 
     it("should work", function() {
         this.timeout(60000)
-        grib2.isIn([{lat:48.487486,long:-4.526367}, {lat:48.893615,long:8.261719}, {lat:44.056012,long:7.734375}, {lat:43.389082,long:-1.669922}], {lat:50.007739,long:1.494141}).should.be.false
-        grib2.isIn([{lat:48.487486,long:-4.526367}, {lat:48.893615,long:8.261719}, {lat:44.056012,long:7.734375}, {lat:43.389082,long:-1.669922}], {lat:45.767523,long:2.460938}).should.be.true
+        grib2.isIn([{lat:48.487486,lon:-4.526367}, {lat:48.893615,lon:8.261719}, {lat:44.056012,lon:7.734375}, {lat:43.389082,lon:-1.669922}], {lat:50.007739,lon:1.494141}).should.be.false
+        grib2.isIn([{lat:48.487486,lon:-4.526367}, {lat:48.893615,lon:8.261719}, {lat:44.056012,lon:7.734375}, {lat:43.389082,lon:-1.669922}], {lat:45.767523,lon:2.460938}).should.be.true
     })
 
 
@@ -19,7 +19,7 @@ describe("Gathering data from file", function() {
 
     it("should work", function(done) {
         this.timeout(60000)
-        grib2.getDataFromFile("/home/hostalerye/Documents/ICC/PE/grib2js/out.csv", [{lat:44.653024,long:-1.625977}, {lat:44.933696,long:3.164063}, {lat:42.55308,long:3.339844}, {lat:43.325178,long:-2.285156}], function(err, res) {
+        grib2.getDataFromFile("/home/hostalerye/Documents/ICC/PE/grib2js/out.csv", [{lat:44.653024,lon:-1.625977}, {lat:44.933696,lon:3.164063}, {lat:42.55308,lon:3.339844}, {lat:43.325178,lon:-2.285156}], function(err, res) {
                 if(err) throw err
                 done()
             }
